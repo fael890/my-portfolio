3e2c14045e869d71f858b2aa84f04b4fe6f0e57f
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-15 10:11:30 -0300

    feat: add animated divider

---
+++ src/components/Divider.tsx
@@ -0,0 +1,8 @@
+import dividerImage from '../assets/divider.png';
+import './Divider.css';
+
+export default function Divider() {
+    return (
+        <div className="divider-container"></div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './root.css'
 import Header from './components/Header'
 import Banner from './components/Banner'
+import Divider from './components/Divider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
     <Banner />
+    <Divider />
   </StrictMode>,
 )
