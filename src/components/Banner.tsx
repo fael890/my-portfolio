3365f60c78e74a803eb797e91f975106c321af90
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-15 09:35:14 -0300

    feat: add banner component

---
+++ src/components/Banner.tsx
@@ -0,0 +1,24 @@
+import './Banner.css';
+import characterImage from '../assets/banner_character_sizecut.png'
+
+export default function Banner() {
+    return (
+        <div className='banner-container grid grid-cols-12'>
+            <div className='col-span-6 justify-items-start content-center'>
+                <h1 className='heading-font1'>
+                    Desenvolvedor Fullstack
+                </h1>
+                <p className='body-font1'>
+                    Meu nome é Rafael Capodeferro, sou desenvolvedor Fullstack. 
+                    O que mais me encanta na área de tecnologia é a possibilidade de propor 
+                    soluções que resolvam problemas de empresas e pessoas. Para isso, estou 
+                    sempre disposto a aprender e me adaptar rapidamente a novas tecnologias e metodologias, 
+                    buscando atender da melhor forma às necessidades de cada projeto.
+                </p>
+            </div>
+            <div className='col-span-6 justify-items-end content-end'>
+                <img src={characterImage} alt="" />
+            </div>  
+        </div>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './root.css'
 import Header from './components/Header'
+import Banner from './components/Banner'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Header />
+    <Banner />
   </StrictMode>,
 )
