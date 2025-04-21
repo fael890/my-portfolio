e605cfa229feed8b5caa0b01160a7c6e0d5ff28c
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-21 16:30:04 -0300

    style: change icons disposition

--- src/components/TechStack.tsx
+++ src/components/TechStack.tsx
@@ -3,7 +3,7 @@ import { TechStackProps } from '../types/TechStack';
 
 export default function TechStack({ iconsSize, iconsNames }: TechStackProps){
     return (
-        <div className="techstack-content flex gap-3">
+        <div className="techstack-content grid justify-between gap-3 grid-cols-6">
             {iconsNames.map((iconName, index) => (
                 <Icon key={index} icon={iconName} width={iconsSize} height={iconsSize} />
             ))}
