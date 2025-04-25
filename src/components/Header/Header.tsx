13276c1b520aff5ff5cc5c15034fd901191f8cf8
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-25 20:06:41 -0300

    style: fix header responsive design

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,25 @@
-import './Header.css';
-
 import { Icon } from "@iconify/react";
 
 const Header = () => {
     return (
         <header className='
-                header-container heading-font3 flex justify-between items-center
-                bg-(--primary-color) text-(--color-white)
-                px-[36px] w-full h-[80px] gap-x-20
+                text-(length:--heading3-font-size) font-(family-name:--heading-family) text-(--color-white)
+                flex justify-between items-center bg-(--primary-color)
+                px-[16px] sm1:px-[36px] w-full h-[80px] gap-x-20
             '
         >
             <div className='hidden md1:flex flex-1 flex justify-start'>
                 <h3 className='hidden md1:flex'>Rafael Capodeferro</h3>
             </div>
-            <div className='justify-center sm1:justify-start md1:justify-center flex-1 flex gap-x-20'>
-                <Icon icon="mdi:gmail" width="40" />
-                <a href="https://github.com/fael890" target='_blank'><Icon icon="mdi:github" width="40" /></a>
-                <a href='https://www.linkedin.com/in/rafael-capodeferro/' target='_blank'><Icon icon="mdi:linkedin" width="40" /></a>
-                <Icon icon="mdi:phone" width="40" />
+            <div className='justify-between sm1:justify-start md1:justify-center flex-1 flex gap-x-6 sm1:gap-x-20'>
+                <Icon icon="mdi:gmail" className="w-[40px] h-[40px] sm1:w-[40px] sm1:h-[40px]" />
+                <a href="https://github.com/fael890" target='_blank'><Icon icon="mdi:github" className="w-[40px] h-[40px] sm1:w-[40px] sm1:h-[40px]" /></a>
+                <a href='https://www.linkedin.com/in/rafael-capodeferro/' target='_blank'><Icon icon="mdi:linkedin" className="w-[40px] h-[40px] sm1:w-[40px] sm1:h-[40px]" /></a>
+                <Icon icon="mdi:phone" className="w-[40px] h-[40px] sm1:w-[40px] sm1:h-[40px]" />
             </div>
             <div className='flex-1 flex justify-end hidden sm1:flex'>
                 <a href='https://drive.google.com/file/d/1ImurOLZt0HKimFqNy3xCLZWa3KVvU9NZ/view?usp=drive_link' target='_blank' className='flex gap-x-2 hidden sm1:flex'>
-                    <h3>Meu Currículo</h3>
+                    <h3 className="max-[650px]:text-(length:--heading4-font-size)">Meu Currículo</h3>
                 </a>
             </div>
         </header>
