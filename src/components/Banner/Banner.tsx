d346d5e64dac76cd8b906330c8c9f0f9a7e68fda
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-25 18:31:12 -0300

    style: fix responsive design in banner component

--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,22 +1,21 @@
-import './Banner.css';
-
 const Banner = () => {
     return (
         <div className='
-                banner-container 
-                grid grid-cols-1 bg-(--dark-bg-color) w-full h-full pt-[50px] px-[40px] gap-[20px]
-                md1:grid-cols-2 md1:pt-[150px] md1:px-[80px]
+                bg-(--dark-bg-color) w-full h-full grid grid-cols-1 md1:grid-cols-2  
+                pt-[50px] px-[20px] gap-[20px]
+                md1:pt-[150px] md1:px-[80px]
             '
         >
             <div className='
                     col-span-1 justify-items-center text-center
                     md1:justify-items-start md1:content-center md1:text-justify
                 '
-            >
-                <h1 className='max-w-[700px] heading-font1 text-(--primary-color)'>
+            >   
+                <h1 className="text-(length:--heading3-font-size) sm1:text-(length:--heading1-font-size) font-(family-name:--heading-family) text-(--primary-color) max-w-[700px]">
                     Desenvolvedor Fullstack
                 </h1>
-                <p className='body-font1 text-(--white-color) max-w-[700px]'>
+                
+                <p className='text-(length:--body1-font-size) font-(family-name:--body-family) text-(--white-color) max-w-[700px]'>
                     Meu nome é Rafael Capodeferro, sou desenvolvedor Fullstack.
                     O que mais me encanta na área de tecnologia é a possibilidade de propor
                     soluções que resolvam problemas de empresas e pessoas. Para isso, estou
@@ -29,7 +28,7 @@ const Banner = () => {
                     md1:justify-items-end md1:content-end
                 '
             >
-                <img className="max-w-[500px] w-[400px] md1:max-w-[750px] md1:w-[500px]" src="/public/banner_character_sizecut.png" alt="" />
+                <img className="w-[400px] md1:max-w-[750px] md1:w-[500px]" src="/public/banner_character_sizecut.png" alt="" />
             </div>
         </div>
     );
