3da4571cbfcb336e3094c82689f96a8cc5eb7e37
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:40:22 -0300

    feat: add responsive design for curriculum button

--- src/components/CurriculumButton/CurriculumButton.tsx
+++ src/components/CurriculumButton/CurriculumButton.tsx
@@ -3,7 +3,7 @@ import { Icon } from "@iconify/react";
 const CurriculumButton = () => {
     return (
         <a
-            href="/link-do-curriculo.pdf" // Substitua pelo seu link
+            href="https://drive.google.com/file/d/1ImurOLZt0HKimFqNy3xCLZWa3KVvU9NZ/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="flex items-center gap-2 fixed body-font1 top-4 right-4 bg-(--primary-color) text-(--white-color) px-4 py-2 rounded-full shadow-lg sm:hidden z-50"
