9651adf29af23513f526cc861b6d9455406dc4be
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:51:28 -0300

    feat: add responsive design for section title component

--- src/components/SectionTitle/SectionTitle.tsx
+++ src/components/SectionTitle/SectionTitle.tsx
@@ -1,9 +1,19 @@
-import './SectionTitle.css';
-
 const SectionTitle = (props: {title: string} ) => {
     return (
-        <div className='section-title-container flex items-center justify-center'>
-            <h2 className='heading-font2'>{props.title}</h2>
+        // <div className='section-title-container flex items-center justify-center'>
+        //     <h2 className='heading-font2'>{props.title}</h2>
+        // </div>
+        <div className="
+                flex bg-[url(/public/section-title.svg)] bg-no-repeat bg-center items-center justify-center
+                bg-contain h-[80px] w-[250px] sm1:w-full sm1:h-[100px] 
+            "
+        >
+            <h2 className='
+                    text-(--white-color)
+                    text-(length:--heading3-font-size) font-(family-name:--heading-family)
+                    sm1:text-(length:--heading2-font-size) sm1:font-(family-name:--heading-family)
+                '
+            >{props.title}</h2>
         </div>
     );
 }
