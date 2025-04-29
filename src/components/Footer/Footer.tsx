685536c397db9b5d2459d9984407ea2e8b7ef7fe
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:46:14 -0300

    feat: add responsive design for footer component

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -2,17 +2,17 @@ import { Icon } from "@iconify/react";
 
 const Footer = () => {
     return (
-        <div className="footer-container">
+        <div className="footer-container pb-[80px] sm1:pb-0">
             <div className="bg-[url(/city-image.svg)] w-full h-[335px]"></div>
             <div className="flex flex-col items-center py-[60px] bg-[var(--dark-bg-color)] gap-[40px]">
-                <div className="flex flex-row text-[var(--primary-color)] gap-10 mx-[80px]">
+                <div className="sm1:flex flex-row flex-wrap justify-center text-[var(--primary-color)] gap-10 mx-[80px] hidden ">
                     <Icon icon="mdi:gmail" width="40" />
                     <a href="https://github.com/fael890" target='_blank'><Icon icon="mdi:github" width="40" /></a>
                     <a href="https://www.linkedin.com/in/rafael-capodeferro/" target='_blank'><Icon icon="mdi:linkedin" width="40" /></a>
                     <Icon icon="mdi:phone" width="40" />
                 </div>
-                <h3 className="body-font1 text-[var(--primary-color)] italic text-center mx-[80px]">“Só conseguimos ver uma curta distância à frente, mas vemos muito que precisa ser feito.” — Alan Turing</h3>
-                <h3 className="body-font1 text-[var(--primary-color)] mx-[80px] text-center">Design inspirado no trabalho de Arief Rachman.</h3>
+                <h3 className="body-font1 text-[var(--primary-color)] italic text-center px-[20px] sm1:px-[40px] md1:px-[80px]">“Só conseguimos ver uma curta distância à frente, mas vemos muito que precisa ser feito.” — Alan Turing</h3>
+                <h3 className="body-font1 text-[var(--primary-color)] px-[20px] sm1:px-[40px] md1:px-[80px] text-center">Design inspirado no trabalho de Arief Rachman.</h3>
             </div>
         </div>
     );
