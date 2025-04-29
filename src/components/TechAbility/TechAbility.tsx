b3e6497094549a94df5b9f3427f0ea312456faba
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:52:07 -0300

    feat: add resposive design for tech ability component

--- src/components/TechAbility/TechAbility.tsx
+++ src/components/TechAbility/TechAbility.tsx
@@ -1,17 +1,20 @@
 import { Icon } from "@iconify/react";
 import { TechAbilityProps } from "../../types/TechAbility";
 
-const TechAbility = ({ iconName, iconLabel, techDocLink } : TechAbilityProps) => {
+const TechAbility = ({ iconName, iconLabel, techDocLink }: TechAbilityProps) => {
     function accessOnClick(techDocLink: string) {
         window.open(techDocLink, '_blank');
     }
 
     return (
-        <div className="tech-ability-container flex w-fit flex-col p-2 items-center">
-            <div className="icon-container p-2" onClick={() => accessOnClick(techDocLink)}>
-                <Icon className="icon-ability" icon={iconName} width="90" height="90"/>
+        <div className="flex flex-col items-center max-w-[120px] "> 
+            <div className="hover:cursor-pointer bg-(--primary-color) rounded-[5px] p-2" onClick={() => accessOnClick(techDocLink)}>
+                <Icon
+                    className="text-(--dark-bg-color) w-16 h-16 sm1:w-20 sm1:h-20 md1:w-30 md1:h-30"
+                    icon={iconName}
+                />
             </div>
-            <h3 className="tech-title heading-font4">{iconLabel}</h3>
+            <h3 className="text-white tech-title text-(length:--heading4-font-size) font-(family-name:--heading-family) text-center">{iconLabel}</h3>
         </div>
     );
 }
