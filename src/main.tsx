6114055558a5497c461cbe575420483612ac837a
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:54:51 -0300

    feat: add resposive design for main.tsx and refactor code

--- src/main.tsx
+++ src/main.tsx
@@ -9,20 +9,28 @@ import techAbilities from './data/techAbility.json'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <div className='hidden sm1:block'>
-      <Header/>
+      <Header />
     </div>
 
-    <CurriculumButton/>
+    <CurriculumButton />
 
     <Banner />
     <Divider />
 
-    <div className='flex-1 sm:py-[30px] md:py-[20px] lg:py-[60px] justify-items-center'>
+    <div className='flex-1 px-[20px] py-[30px] sm1:px-[40px] sm1:py-[30px] md1:px-[80px] md1:py-[40px] justify-items-center'>
       <SectionTitle title='Projetos' />
     </div>
-    <div className='grid justify-between lg:grid-cols-2 md:grid-cols-2 sm:grid-cols-1 sm:gap-5 md:gap-10 lg:gap-20 sm:px-[20px] md:px-[50px] lg:px-[80px]'>
-      {projectCardData.map(data => (
+    <div className='
+        grid grid-cols-1 justify-between gap-5 px-[20px]
+        sm1:grid-cols-1 sm1:gap-10 sm1:px-[40px]
+        sm2:grid-cols-2 sm2:gap-10 sm2:px-[40px]
+        min-[750px]:grid-cols-1 min-[750px]:grid-cols-1
+        md1:grid-cols-2 md1:gap-20 md1:px-[80px]  
+      '
+    >
+      {projectCardData.map((data, index) => (
         <ProjectCard
+          key={index}
           title={data.title}
           description={data.description}
           projectLink={data.projectLink}
@@ -32,27 +40,28 @@ createRoot(document.getElementById('root')!).render(
       ))}
     </div>
 
-    <div className='tech-ability-container my-[60px] pt-[60px] pb-[60px]'>
-      <div className='flex-1 justify-items-center lg:mx-[80px] pb-[60px]'>
+    <div className='bg-(--dark-bg-color) mt-[30px] md1:mt-[40px] px-[20px] py-[30px] sm1:px-[40px] sm1:py-[30px] md1:px-[80px] md1:py-[40px]'>
+      <div className='flex-1 justify-items-center pb-[30px] sm1:pb-[30px] md1:pb-[40px]'>
         <SectionTitle title='Habilidades' />
       </div>
-      <div className='flex justify-items-center gap-10 grid-cols-6 lg:mx-[80px] flex-wrap'>
-        {techAbilities.map(data => (
-          <div>
-            <TechAbility
-              iconName={data.iconName}
-              iconLabel={data.iconLabel}
-              techDocLink={data.techDocLink}
-            />
-          </div>
+      <div className='grid max-[400px]:grid-cols-2 grid-cols-3 sm1:grid-cols-5 md1:grid-cols-6 justify-items-center gap-5 sm1:gap-10 md1:gap-20'>
+        {techAbilities.map((data, index) => (
+          <TechAbility
+            key={index}
+            iconName={data.iconName}
+            iconLabel={data.iconLabel}
+            techDocLink={data.techDocLink}
+          />
         ))}
       </div>
     </div>
 
-    <Footer />
+    <div className='mt-[60px] md1:mt-[80px]'>
+      <Footer />
+    </div>
 
     <div className='fixed bottom-0 w-full sm1:hidden'>
-      <Header/>
+      <Header />
     </div>
 
   </StrictMode>,
