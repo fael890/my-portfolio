e8dd9690c9b2c5b7158470c32258f169da5337a9
Rafael Capodeferro <rafael222.rc@gmail.com>
2025-04-29 17:52:44 -0300

    feat: add resposive design for tech stack component

--- src/components/TechStack/TechStack.tsx
+++ src/components/TechStack/TechStack.tsx
@@ -3,7 +3,7 @@ import { TechStackProps } from '../../types/TechStack';
 
 const TechStack = ({ iconsSize, iconsNames }: TechStackProps) => {
     return (
-        <div className="techstack-content grid justify-between gap-3 grid-cols-6">
+        <div className="text-(--primary-color) grid justify-between gap-3 grid-cols-6">
             {iconsNames.map((iconName, index) => (
                 <Icon key={index} icon={iconName} width={iconsSize} height={iconsSize} />
             ))}
